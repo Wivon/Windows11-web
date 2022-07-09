86b4ef53100b8adb6d797fc45ae331b9a1718a9f
Wivon <brousselilian6@gmail.com>
2022-07-09 17:22:15 +0200

    🛠️work: recommended apps & files

--- startMenu.js
+++ startMenu.js
@@ -19,10 +19,16 @@ const PINNED_APPS = [
     { name: 'Discord', icon: 'assets/discord-icon.png'}
 ]
 
+RECOMENDED_ITEMS = [
+    { name: 'Spotify', icon: 'assets/spotify-icon.png', time: '25min ago'},
+    { name: 'Segoe UI', icon: 'assets/discord-icon.png', time: 'Yesterday at 17:18'}
+]
+
 class startMenu extends HTMLElement {
     constructor() {
         super()
         this.renderPinnedApps()
+        this.renderRecommendedItems()
     }
 
     renderPinnedApps() {
@@ -30,8 +36,23 @@ class startMenu extends HTMLElement {
             let newItem = document.createElement('div')
             let container = this.querySelector('.pinned .container')
             newItem.innerHTML = `
-                <img src="${app.icon}">
+                <img draggable="false" src="${app.icon}">
                 <span>${app.name}</span>
+            `
+            container.appendChild(newItem)
+        })
+    }
+
+    renderRecommendedItems() {
+        RECOMENDED_ITEMS.forEach(app => {
+            let newItem = document.createElement('div')
+            let container = this.querySelector('.recommendations .container')
+            newItem.innerHTML = `
+                <img draggable="false" src="${app.icon}">
+                <div class="infos">
+                    <span>${app.name}</span>
+                    <span>${app.time}</span>
+                </div>
             `
             container.appendChild(newItem)
         })
